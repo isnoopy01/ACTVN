1c95484a45ec3ce68997b83e5de67c8ab9c8c97e
mikequinnn <maiquynhanhhh1302@gmail.com>
2024-04-06 17:41:34 +0700

    style - homescreen 06042024

--- src/screen/home/homeScreen.jsx
+++ src/screen/home/homeScreen.jsx
@@ -1,14 +1,28 @@
-import {Text, View } from 'react-native'
-import React from 'react'
-import styles from './styles'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import {Text, View, Image} from 'react-native';
+import React from 'react';
+import styles from './styles';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 const HomeScreen = () => {
-    return (
-        <SafeAreaView>
-            <Text>hêlo</Text>
-        </SafeAreaView>
-    )
-}
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <View style={styles.header1}>
+            <Image style={{ width: 37, height: 37 }} source = {require('../../img/avatar.png')}/>
+            <View style={styles.info}>
+                <View style={styles.names}>
+                    <Text style={styles.name}>Mai Thị Quỳnh</Text>
+                    <Text style={styles.codeStudent}>AT170341</Text>
+                </View>
+                <Text style={styles.major}>An toàn thông tin</Text>
+            </View>
+        </View>
+        <View>
+            <Image style={{ width: 30, height: 30 }}  source = {require('../../img/bell.png')}/>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
 export default HomeScreen;
--- src/screen/home/styles.jsx
+++ src/screen/home/styles.jsx
@@ -3,7 +3,39 @@ import { StyleSheet } from "react-native";
 
 
 const styles = StyleSheet.create({
-    
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingLeft: 20,
+        paddingBottom: 10,
+        paddingTop: 10,
+        paddingRight: 20,
+        alignItems: 'center'
+    },
+    info: {
+        marginLeft: 10
+    },
+    header1: {
+        flexDirection: 'row',
+    },
+    names: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    name: {
+        paddingRight: 10,
+        // borderRightColor: 'black',
+        // borderStyle: 'solid',
+        borderRightColor: 'black',
+        borderRightWidth: 1,
+        fontSize: 18,
+        fontWeight: 'bold'
+
+    },
+    codeStudent: {
+        fontSize: 16,
+        fontWeight: 'normal'
+    }
 
 
 })
