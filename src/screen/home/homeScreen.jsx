5ed9a7ff2ebb0d02a045c8c7ffe168b7513c5214
mikequinnn <maiquynhanhhh1302@gmail.com>
2024-04-06 16:16:51 +0700

    tab - homescreen - 06042024

--- src/screen/home/homeScreen.jsx
+++ src/screen/home/homeScreen.jsx
@@ -1,12 +1,13 @@
 import {Text, View } from 'react-native'
 import React from 'react'
 import styles from './styles'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const HomeScreen = () => {
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView>
+            <Text>hêlo</Text>
+        </SafeAreaView>
     )
 }
 
